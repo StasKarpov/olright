b0c592841bd45182da83ec834b5a189f6cd434b3
Stas Karpov <karpovstanislav1@gmail.com>
2022-02-05 14:46:17 +0100

    use hash router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import { LangProvider } from "./context/lang";
 import { TRANSLATION } from "./queries/translation";
 import Query from "./components/Query";
